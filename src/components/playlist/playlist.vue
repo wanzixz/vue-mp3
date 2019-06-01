d96e240fe72f2fbe782ba9c349c61754ac7b6669
project <1471299010@qq.com>
2019-06-01 08:10:30 +0800

    Update playlist.vue
    修复transition-group

--- src/components/playlist/playlist.vue
+++ src/components/playlist/playlist.vue
@@ -11,7 +11,7 @@
         </div>
         <scroll :refreshDelay="refreshDelay" ref="listContent"  class="list-content" >
           <transition-group ref="list" name="list" tag="ul">
-            <li class="item" v-for="(item, index) in sequenceList" :key="index" @click="selectItem(item, index)">
+            <li class="item" v-for="(item, index) in sequenceList" v-bind:key="index" @click="selectItem(item, index)">
               <i class="current" :class="getCurrentIcon(item)"></i>
               <span class="text">{{item.name}}</span>
               <span class="like" @click.stop="toggleFavorite(item)">
